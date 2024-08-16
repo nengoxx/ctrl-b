a7030ab21da1626a1be180132bc6a34ce415367b
nengoxx <obitoyelmaki@hotmail.com>
2024-08-16 22:14:46 +0200

    updated for raspbian

--- install.sh
+++ install.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 VENV_FOLDER=".venv"
-PYTHON_VERSION="python3.11"
+PYTHON_VERSION="python3.9"
 
 # Check if the virtual environment already exists
 if [ ! -d "$VENV_FOLDER" ]; then
     echo "Creating virtual environment..."
     $PYTHON_VERSION -m venv $VENV_FOLDER
+    
+    # Activate the virtual environment
+    source $VENV_FOLDER/bin/activate
+
+    # Install the requirements
+    echo "Installing requirements..."
+    pip install -r requirements.txt
 else
     echo "Virtual environment already exists."
+
+    source $VENV_FOLDER/bin/activate
 fi
 
-# Activate the virtual environment
-source $VENV_FOLDER/bin/activate
 
-# Install the requirements
-echo "Installing requirements..."
-pip install -r requirements.txt
 
 # Keep the terminal open
 echo "Virtual environment is activated."
