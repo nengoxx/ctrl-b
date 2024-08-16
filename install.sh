ae8cb972652760f2f67abc4dfa97ed5c300a30e3
nengoxx <obitoyelmaki@hotmail.com>
2024-08-16 20:44:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-VENV_FOLDER=.venv
-PYTHON_VERSION=3.11
+VENV_FOLDER=".venv"
+PYTHON_VERSION="python3.11"
 
-if [! -d "$VENV_FOLDER" ]; then
-    python -m venv $VENV_FOLDER --python=$PYTHON_VERSION
+# Check if the virtual environment already exists
+if [ ! -d "$VENV_FOLDER" ]; then
+    echo "Creating virtual environment..."
+    $PYTHON_VERSION -m venv $VENV_FOLDER
+else
+    echo "Virtual environment already exists."
 fi
 
+# Activate the virtual environment
 source $VENV_FOLDER/bin/activate
 
+# Install the requirements
+echo "Installing requirements..."
 pip install -r requirements.txt
 
-# Keep terminal open
-bash
+# Keep the terminal open
+echo "Virtual environment is activated."
+exec "$SHELL"
